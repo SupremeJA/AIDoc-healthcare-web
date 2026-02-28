5481c032c9c54e4dce7e6ecc1d77dca02ef765a8
Joshua Afolabi <telvinamin15@gmail.com>
2026-02-28 01:45:04 +0100

    first commit

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,26 @@
+function Hero() {
+  return (
+    <section class="">
+      <div className="bg-accent text-secondary text-sm font-semibold">
+        AI Healthcare Service
+      </div>
+
+      <div>
+        <div className="text-black">
+          <p>
+            Experience Smart, AI-Driven{" "}
+            <span className="text-accent">Healthcare</span>
+          </p>
+          <p>for Faster, Better Care!</p>
+          <p className="text-gray-700 text-sm">
+            Get real-time medical advice, accurate diagnostics, and personalized
+            health insights powered by advanced AI technology - accessible
+            anytime, anywhere.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
