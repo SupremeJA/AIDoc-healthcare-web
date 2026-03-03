e86ee2932c52df599cf36e4f7062dbf4f858fb09
Joshua Afolabi <telvinamin15@gmail.com>
2026-03-03 02:55:54 +0100

    update

--- src/components/ServiceList.jsx
+++ src/components/ServiceList.jsx
@@ -2,24 +2,30 @@ import {
   ArrowUpRightIcon,
   HospitalIcon,
   CheckCircleIcon,
+  HeadsetIcon,
 } from "@phosphor-icons/react";
 import data from "../data/data.json";
 
 const ICONS = {
   HospitalIcon,
   CheckCircleIcon,
+  ArrowUpRightIcon,
+  HeadsetIcon,
 };
 
 function ServiceList() {
   return (
-    <div>
+    <>
       {data.services.map((item) => {
         const Icon = ICONS[item.icon];
         return (
-          <div className="flex md:flex-row items-center gap-20 p-4 bg-gray-50 text-black rounded-2xl">
-            <div className="flex justify-center items-center gap-2">
+          <div className="flex flex-col md:flex-row items-center justify-between p-8 md:p-4 bg-gray-50 text-black rounded-2xl">
+            <div className="flex flex-col md:flex-row md:justify-center md:items-center gap-2">
               {Icon && (
-                <Icon className="text-accent bg-lime-100 p-1 w-13 h-full rounded-full" />
+                <Icon
+                  size={20}
+                  className={`text-${item.color} bg-${item.color}/20 w-12 h-12 p-2 rounded-full`}
+                />
               )}
               <div>
                 <h3 className="text-sm">{item.title}</h3>
@@ -29,12 +35,12 @@ function ServiceList() {
 
             <ArrowUpRightIcon
               size={20}
-              className="hover:bg-accent text-black w-10 h-10 p-2 rounded-full"
+              className="hidden md:block hover:bg-accent text-black w-10 h-10 p-2 rounded-full"
             />
           </div>
         );
       })}
-    </div>
+    </>
   );
 }
 
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,16 +1,15 @@
-import { ArrowUpRightIcon } from "@phosphor-icons/react";
 import ServiceList from "../components/ServiceList";
 
 function Services() {
   return (
     // Check later
-    <section className="flex justify-center items-center flex-col gap-8 max-w-4xl mx-auto mt-20">
+    <section className="flex justify-center items-center flex-col gap-8 max-w-4xl mx-auto px-5 mt-20">
       <header className="text-center">
         <h2>Our Service</h2>
         <p className="mt-2">Explore Expert Insights and Health Tips</p>
       </header>
 
-      <div>
+      <div className="flex flex-col gap-3 ">
         <ServiceList />
       </div>
     </section>
