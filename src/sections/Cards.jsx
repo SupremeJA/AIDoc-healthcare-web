09cc6f28699a990d3b9b4dbbe5627f8571ce99b6
Joshua Afolabi <telvinamin15@gmail.com>
2026-03-04 02:43:43 +0100

    update: card(items-stretch)

--- src/sections/Cards.jsx
+++ src/sections/Cards.jsx
@@ -2,7 +2,7 @@ import List from "../components/List";
 
 function Cards() {
   return (
-    <section className="grid! grid-cols-1! md:grid-cols-3! items-start! gap-4!">
+    <section className="grid! grid-cols-1! md:grid-cols-3! items-stretch! gap-4!">
       <List cards={true} />
     </section>
   );
