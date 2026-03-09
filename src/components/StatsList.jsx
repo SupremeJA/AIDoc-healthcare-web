5ac324d8bea38d0962155ac0801462f9dd266298
Joshua Afolabi <telvinamin15@gmail.com>
2026-03-09 18:02:53 +0100

    update: statslist to flex-col on mobile

--- src/components/StatsList.jsx
+++ src/components/StatsList.jsx
@@ -2,9 +2,9 @@ import data from "../data/data.json";
 
 function StatsList() {
   return (
-    <div className="flex flex-row md:flex-row justify-between bg-green-500 w-full max-w-4xl mx-auto ">
+    <div className="flex flex-col md:flex-row justify-between w-full max-w-4xl mx-auto ">
       {Object.entries(data.stats).map(([key, value]) => (
-        <div className="p-6 rounded-xl bg-white">
+        <div className="p-6 rounded-xl bg-white text-center">
           <h3>{key}</h3>
           <p>{value}</p>
         </div>
